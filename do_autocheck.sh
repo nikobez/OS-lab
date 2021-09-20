481e4d671a0528c60e58f7434cf32226268e5157
Nick Druzhinin <nikobez@yandex.ru>
2021-09-20 06:24:36 +0000

    Clearing known_hosts added

--- do_autocheck.sh
+++ do_autocheck.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -e ~/.ssh/known_hosts ]; then
+    echo ""
+    echo "Clear known hosts ..."
+    rm ~/.ssh/known_hosts
+fi
+
 addresses=$(who)
 
 for addr in $(who | awk -F' ' '/lab/ {print $1 $5}'); do
